11b0e241b3877a2eae25d42f77690d8746837222
Aslak Hollund <aslakhol@gmail.com>
2020-09-10 18:00:31 +0200

    Updates

---
+++ precache-manifest.4f989378f0fc792cc10743165ad56d45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c13b696554c07b5f61102b31a642db2",
+    "url": "/infoskjerm-kurs/index.html"
+  },
+  {
+    "revision": "83132a8752875c2b6134",
+    "url": "/infoskjerm-kurs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b1dee396bed78d5c0038",
+    "url": "/infoskjerm-kurs/static/js/2.7b06c8a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm-kurs/static/js/2.7b06c8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83132a8752875c2b6134",
+    "url": "/infoskjerm-kurs/static/js/main.a6a85996.chunk.js"
+  },
+  {
+    "revision": "163087e278b967875cbf",
+    "url": "/infoskjerm-kurs/static/js/runtime-main.ded50dbf.js"
+  }
+]);
